726541cf45402bbebb198486f8374df1f55c1885
VeryFunnyMonkey <VeryFunnyMonkey@users.noreply.github.com>
2025-01-31 12:56:42 +0800

    fixed formatting, added tooltip for delete mode

--- DateIdeas.Frontend/src/components/FilterDropdown.jsx
+++ DateIdeas.Frontend/src/components/FilterDropdown.jsx
@@ -9,9 +9,9 @@ export default function FilterDropdown({ tags, selectedCategories, setSelectedCa
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
+
   const handleDeleteTagSelect = (tag) => {
     if (isDeleteMode) {
-      console.log(isDeleteMode);
       const tagExists = tagsToDelete.some((selectedTag) => selectedTag.name === tag.name);
       if (tagExists) {
         setTagsToDelete(tagsToDelete.filter((selectedTag) => selectedTag.name !== tag.name));
@@ -22,12 +22,12 @@ export default function FilterDropdown({ tags, selectedCategories, setSelectedCa
   };
 
   const handleFilterTagSelect = (tag) => {
-      const tagExists = selectedFilterTags.some((selectedTag) => selectedTag.name === tag.name);
-      if (tagExists) {
-        setSelectedFilterTags(selectedFilterTags.filter((selectedTag) => selectedTag.name !== tag.name));
-      } else {
-        setSelectedFilterTags([...selectedFilterTags, tag]);
-      }
+    const tagExists = selectedFilterTags.some((selectedTag) => selectedTag.name === tag.name);
+    if (tagExists) {
+      setSelectedFilterTags(selectedFilterTags.filter((selectedTag) => selectedTag.name !== tag.name));
+    } else {
+      setSelectedFilterTags([...selectedFilterTags, tag]);
+    }
   };
 
   const handleCategorySelect = (category) => {
@@ -40,10 +40,8 @@ export default function FilterDropdown({ tags, selectedCategories, setSelectedCa
 
   const toggleDeleteMode = () => {
     if (isDeleteMode && tagsToDelete.length > 0) {
-      console.log("tags", tags);
       setSelectedFilterTags(selectedFilterTags.filter(tag => !tagsToDelete.includes(tag.name)));
       tagsToDelete.forEach((tag) => {
-        console.log(tag);
         deleteTag(tag.id);
         setSelectedFilterTags(selectedFilterTags.filter((selectedTag) => selectedTag.name !== tag.name));
       });
@@ -53,8 +51,8 @@ export default function FilterDropdown({ tags, selectedCategories, setSelectedCa
   };
 
   const categoryIcons = {
-    unscheduled: <LightBulbIcon className="h-5 w-5 mx-auto" />, 
-    scheduled: <CalendarIcon className="h-5 w-5 mx-auto" />, 
+    unscheduled: <LightBulbIcon className="h-5 w-5 mx-auto" />,
+    scheduled: <CalendarIcon className="h-5 w-5 mx-auto" />,
     completed: <CheckCircleIcon className="h-5 w-5 mx-auto" />
   };
 
@@ -69,21 +67,21 @@ export default function FilterDropdown({ tags, selectedCategories, setSelectedCa
       </button>
       {isDropdownOpen && (
         <div className="absolute top-full mt-2 bg-white shadow-lg rounded-lg p-4 w-full max-w-md z-50">
-            <div className='bg-slate-100 rounded-lg p-4 mb-4'>
-              <h2 className="text-lg font-semibold text-gray-700 mb-2">Filter by Category</h2>
-              <div className="grid grid-cols-3 gap-2 mb-4 text-center">
-                {['unscheduled', 'scheduled', 'completed'].map((category) => (
-                  <button
-                    key={category}
-                    onClick={() => handleCategorySelect(category)}
-                    className={`px-4 py-2 rounded-lg text-sm font-medium flex flex-col items-center transition ${
-                      selectedCategories.includes(category) ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'
-                    }`}
-                  >
-                    {categoryIcons[category]} 
-                    <span className="mt-1">{category.charAt(0).toUpperCase() + category.slice(1)}</span>
-                  </button>
-                ))}
+          <div className='bg-slate-100 rounded-lg p-4 mb-4'>
+            <h2 className="text-lg font-semibold text-gray-700 mb-2">Filter by Category</h2>
+            <div className="grid grid-cols-3 gap-2 mb-4 text-center">
+              {['unscheduled', 'scheduled', 'completed'].map((category) => (
+                <button
+                  key={category}
+                  onClick={() => handleCategorySelect(category)}
+                  className={`px-4 py-2 rounded-lg text-sm font-medium flex flex-col items-center transition ${
+                    selectedCategories.includes(category) ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'
+                  }`}
+                >
+                  {categoryIcons[category]}
+                  <span className="mt-1">{category.charAt(0).toUpperCase() + category.slice(1)}</span>
+                </button>
+              ))}
             </div>
           </div>
           <div className='bg-slate-100 rounded-lg p-4 mb-4'>
@@ -93,6 +91,11 @@ export default function FilterDropdown({ tags, selectedCategories, setSelectedCa
                 {isDeleteMode ? <CheckIcon className="h-5 w-5" /> : <TrashIcon className="h-5 w-5" />}
               </button>
             </div>
+            {isDeleteMode && (
+              <div className="text-sm text-gray-500 mb-2">
+                Tap a tag to mark it for delete, tap the delete button again to delete the marked tags.
+              </div>
+            )}
             <div className="flex gap-2 flex-wrap">
               {tags.map((tag) => (
                 <span
